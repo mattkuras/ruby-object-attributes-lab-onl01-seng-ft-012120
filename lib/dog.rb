29d3cf9cb047023386bdc36ec6d4595bf8889850
Matt Kuras <mattkuras@gmail.com>
2020-01-31 19:52:19 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,36 @@
+class Dog 
+def name=(dog_name)
+  @name = dog_name
+end
+
+def name
+  @name 
+end
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+
+def breed
+  @breed
+end
+end
+
+class Person
+  def name=(person_name)
+    @name = person_name
+end 
+
+def name 
+  @name 
+end
+
+def job=(persons_job)
+  @job = persons_job
+end
+
+def job
+  @job
+end
+
+end
